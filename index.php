a011539fe540016af108b6841fb7743770851422
GianmarcoDandrea <gianm96@hotmail.it>
2023-12-11 15:38:30 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <form action="censored.php" method="GET">
+        <label for="paragraph">Il Tuo Paragrafo</label>
+        <input type="text" id="paragraph" name="paragraph">
+
+        <label for="censored-word">La parola da censurare</label>
+        <input type="text" id="censored_word" name="censored_word">
+
+        <button type="submit">Invia</button>
+    </form>
+
+</body>
+</html>
