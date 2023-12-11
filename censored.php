1b7bc08e9e6982321d120629074bc20e5a07b418
GianmarcoDandrea <gianm96@hotmail.it>
2023-12-11 16:19:19 +0100

    formated the documents

--- censored.php
+++ censored.php
@@ -9,41 +9,38 @@ $censored_paragraph_length = strlen($censored_paragraph);
 
 <!DOCTYPE html>
 <html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+  </head>
 
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-    </head>
+  <body>
+    <section>
+      <p>
+        Il paragrafo da te inseririto è <br />
+        <?php echo $paragraph ?>
+      </p>
 
-    <body>
-        <section>
-            <p>
-                Il paragrafo da te inseririto è <br>
-                <?php echo $paragraph ?>
-            </p>
-    
-            <p>
-                La lunghezza del paragrafo è: <?php echo $paragraph_length; ?>
-            </p>
+      <p>
+        La lunghezza del paragrafo è:
+        <?php echo $paragraph_length; ?>
+      </p>
+    </section>
 
-        </section>
+    <br />
+    <br />
 
-        <br>
-        <br>
-        
-        <section>
-            <p> 
-                Il paragrafo con le parole censurate è: <br>
-                <?php echo $censored_paragraph ?>
-            </p>
+    <section>
+      <p>
+        Il paragrafo con le parole censurate è: <br />
+        <?php echo $censored_paragraph ?>
+      </p>
 
-            <p>
-                La lunghezza del paragrafo è: <?php echo $censored_paragraph_length; ?>
-            </p>
-
-        </section>
-
-    </body>
-
+      <p>
+        La lunghezza del paragrafo censurato è:
+        <?php echo $censored_paragraph_length; ?>
+      </p>
+    </section>
+  </body>
 </html>
--- index.php
+++ index.php
@@ -1,23 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Document</title>
-</head>
-<body>
-
+  </head>
+  <body>
     <form action="censored.php" method="GET">
+      <label for="paragraph">Il Tuo Paragrafo</label>
+      <input type="text" id="paragraph" name="paragraph" />
 
-        <label for="paragraph">Il Tuo Paragrafo</label>
-        <input type="text" id="paragraph" name="paragraph">
+      <label for="censored_word">La parola da censurare</label>
+      <input type="text" id="censored_word" name="censored_word" />
 
-        <label for="censored_word">La parola da censurare</label>
-        <input type="text" id="censored_word" name="censored_word">
-
-        <button type="submit">Invia</button>
-        
+      <button type="submit">Invia</button>
     </form>
-
-</body>
+  </body>
 </html>
